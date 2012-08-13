6b4dafee1f3312ddee64350bc2677d21a7bc8560
weed <weed_7777@yahoo.co.jp>
2012-08-13 17:04:09 +0900

    Initial Commit

---
+++ UIPickerViewTitleRow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIPickerViewTitleRow
+//
+//  Created by 達郎 植田 on 12/08/13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIPickerViewTitleRow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIPickerViewTitleRow
+//
+//  Created by 達郎 植田 on 12/08/13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
